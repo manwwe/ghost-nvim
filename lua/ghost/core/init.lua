55bf58701f0e4959287dc67974379b69bd34b76d
Franklin Garcia <fm-garcia@outlook.com>
2026-03-11 11:35:39 -0500

    refactor: reorganise lua config structure

--- lua/ghost/core/init.lua
+++
@@ -1,3 +0,0 @@
-require("ghost.core.options")
-require("ghost.core.keymaps")
-require("ghost.core.autocmds")
--- lua/ghost/plugins/init.lua
+++
@@ -1,7 +0,0 @@
--- plugins/init.lua
--- Entry point for all plugins loaded via Lazy.nvim.
-
-return {
-  { import = "ghost.plugins.lsp" },
-  { import = "ghost.plugins.treesitter" },
-}
